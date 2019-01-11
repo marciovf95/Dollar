f3fe286044e320d5c785b7eb1416d7620bb849f1
Marcio <marciovf95@gmail.com>
2019-01-11 17:44:38 -0200

    .

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import cx_Oracle 
 import myrequire
-myrequire.require_once('enviaremail')
+import enviaremail
 
 # python -m pip install cx_Oracle --upgrade
 # https://cx-oracle.readthedocs.io/en/latest/installation.html
@@ -10,7 +10,7 @@ def open():
 		
 		cursor = con.cursor()		
 	except:
-		myrequire.enviaremail.sendmail("BO ao Abrir connect com o Banco")
+		enviaremail.sendmail("BO ao Abrir connect com o Banco")
 	finally:
   		print("The 'try except' is finished")
     return cursor 
@@ -20,7 +20,7 @@ def close():
 	try:
 		con.close()
 	except:
-		myrequire.enviaremail.sendmail("BO do Boroguido")
+		enviaremail.sendmail("BO do Boroguido")
 
 
 
