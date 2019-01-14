4f46e7233520aa2b9fe2c95862e10f780a3a1b8a
Marcio <marciovf95@gmail.com>
2019-01-14 15:12:38 -0200

    Mais Testes

--- dollar.py
+++ dollar.py
@@ -8,31 +8,36 @@ import time
 
 try:	
 	# con = cx_Oracle.connect('usuario/senha@ip:porta/schema')
-		
+	
 	cursor = con.cursor()		
 except:
 	enviaremail.sendmail("BO ao Abrir connect com o Banco")
 
 def getDollar():
-	with urllib.request.urlopen('https://www.valor.com.br/valor-data') as response:
-		html = response.read()
+	try: 
+		with urllib.request.urlopen('https://www.valor.com.br/valor-data') as response:
+			html = response.read()
 
-	soup = BeautifulSoup(html,'html.parser') #armazena o conteudo em html
-	ips = soup.find(id="ticker-data").find_all("span") # procura em toda a HTML a Div Ticker-data dps procuras as Span dentro desta div
-	patx = ips[2].get_text() # Pega o texto da 2 span
-	abc = str(patx).replace(",",".")
-	return abc
+		soup = BeautifulSoup(html,'html.parser') #armazena o conteudo em html
+		ips = soup.find(id="ticker-data").find_all("span") # procura em toda a HTML a Div Ticker-data dps procuras as Span dentro desta div
+		patx = ips[3].get_text() # Pega o texto da 2 span
+		abc = str(patx).replace(",",".")
+		teste = float(abc)
+		return teste
+	except Exception as e:
+		s = str(e)
+		enviaremail.sendmail("Erro "+s+" <--")
+		exit()
 
 
 def salvarDollar(parametro): 
-	cursor_interno = con.cursor()
-	# temp = float(parametro)	
+	cursor_interno = con.cursor()	
 	sql = "INSERT INTO PADOPY_DOLLAR_API (VALOR) VALUES (TO_NUMBER({}))".format(parametro)
+	enviaremail.sendmail("Novo Dollar Casatrado Valor : "+parametro+" Ptax")
 	print(sql)
 	cursor_interno.execute(sql)	
 	con.commit()
 	return "Salvo"
-	
 
 
 def getDollarSalvo():
@@ -42,36 +47,28 @@ def getDollarSalvo():
 		return "Pendente"
 	else:
 		lista = list(dollarSalvo)
-		return lista	
+		return float(lista[0])	
+
 
 a = getDollarSalvo()
-b = getDollar()
+b = getDollar()	
+
 
 if (a == "Pendente"):
 	salvarDollar(b)
+
 
 a = getDollarSalvo()
 
 
 while True:
 	b = getDollar()
-	if float(a[0]) == float(b):
-		# time.sleep(1800)
-		print(a[0],b)
-		time.sleep(5)
+	if float(a) == float(b):		
+		print(a,b)
+		time.sleep(1800)
 	else:
 		print("Salvando Novo Dollar")
 		salvarDollar(b)
 		a = getDollarSalvo()
 
-
-
-# cur.execute("INSERT INTO PADOPY_DOLLAR_API (VALOR) VALUES ("+abc+")")
-# db.commit()
-# con.close()
-#enviaremail.sendmail("Cotacao Ptax :"+abc)
-
-#PADOPY_DOLLAR_API
-
-# print(abc)
 
